e64d75b38d8f5306fb70ef0f8a8f6aa9b5281e7b
Goulven Traimany <gtraiman@made-f0Ar5s4.clusters.42paris.fr>
2024-09-01 18:40:03 +0200

    final1

---
+++ pipex.h
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gtraiman <gtraiman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 11:25:28 by gtraiman          #+#    #+#             */
+/*   Updated: 2024/09/01 12:55:34 by gtraiman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "libft/libft.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+typedef struct openfile
+{
+	int		infile_fd;
+	int		outfile_fd;
+	int		argc;
+	char	**argv;
+	int		i;
+}					t_openfile;
+
+char	**ft_get_path(char **envp);
+int		ft_access(char **tab, char *avi, char **PATH);
+int		ft_exec(char *argi, char **envp);
+int		main(int ac, char **av, char **envp);
+void	dblclose(int	*pipe);
+int		dblclosin(t_openfile *inout);
+int		openfd(t_openfile *inout);
+int		makeapipe(int *pipefd);
+int		dup_in(pid_t *pid, t_openfile *inout, char **envp, int ppipefd[2]);
+int		last_dup(t_openfile *inout, int i, char **envp);
+int		all_dup(t_openfile inout, char **envp);
+int		ifdup(t_openfile inout, int *pipefd, int i);
+
+#endif
